1e1910e3557ccaf60f3ef7db19113f1934a8466c
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-18 15:01:04 +0900

    :sparkles: :: 토큰 발행 클래스 추가

---
+++ src/main/kotlin/com/gsmNetworking/auth/global/security/jwt/TokenGenerator.kt
@@ -0,0 +1,46 @@
+package com.gsmNetworking.auth.global.security.jwt
+
+import com.gsmNetworking.auth.domain.auth.domain.Authority
+import com.gsmNetworking.auth.global.security.jwt.dto.TokenResponse
+import com.gsmNetworking.auth.global.security.jwt.properties.JwtExpTimeProperties
+import com.gsmNetworking.auth.global.security.jwt.properties.JwtProperties
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.util.*
+
+@Component
+class TokenGenerator(
+    private val jwtProperties: JwtProperties,
+    private val jwtExpTimeProperties: JwtExpTimeProperties
+) {
+
+    fun generateToken(email: String, authority: Authority): TokenResponse =
+        TokenResponse(
+            accessToken = generateAccessToken(email, authority),
+            refreshToken = generateRefreshToken(email),
+            accessTokenExp = LocalDateTime.now().plusSeconds(jwtExpTimeProperties.accessExp.toLong()).withNano(0),
+            refreshTokenExp = LocalDateTime.now().plusSeconds(jwtExpTimeProperties.refreshExp.toLong()).withNano(0),
+        )
+
+    private fun generateAccessToken(email: String, authority: Authority): String =
+        Jwts.builder()
+            .signWith(jwtProperties.accessSecret, SignatureAlgorithm.HS256)
+            .setSubject(email)
+            .claim(JwtProperties.TOKEN_TYPE, JwtProperties.ACCESS)
+            .claim(JwtProperties.AUTHORITY, authority.authority)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + jwtExpTimeProperties.accessExp * 1000))
+            .compact()
+
+    private fun generateRefreshToken(email: String): String =
+        Jwts.builder()
+            .signWith(jwtProperties.refreshSecret, SignatureAlgorithm.HS256)
+            .setSubject(email)
+            .claim(JwtProperties.TOKEN_TYPE, JwtProperties.REFRESH)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + jwtExpTimeProperties.refreshExp * 1000))
+            .compact()
+
+}
