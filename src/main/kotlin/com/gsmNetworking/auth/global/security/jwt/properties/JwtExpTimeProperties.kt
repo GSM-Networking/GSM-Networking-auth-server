43e2796ad8d645fe1622e85c936d936c647806f5
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-18 14:59:16 +0900

    :sparkles: :: jwt properties 추가

---
+++ src/main/kotlin/com/gsmNetworking/auth/global/security/jwt/properties/JwtExpTimeProperties.kt
@@ -0,0 +1,11 @@
+package com.gsmNetworking.auth.global.security.jwt.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties("jwt.time")
+class JwtExpTimeProperties(
+    val accessExp: Int,
+    val refreshExp: Int
+)
