f3e7ee35cfc2ac73875d31dc446d42bec7e3ac4a
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-21 23:16:16 +0900

    :sparkles: :: email indexed로 지정

--- src/main/kotlin/com/gsmNetworking/auth/domain/auth/domain/RefreshToken.kt
+++ src/main/kotlin/com/gsmNetworking/auth/domain/auth/domain/RefreshToken.kt
@@ -3,6 +3,7 @@ package com.gsmNetworking.auth.domain.auth.domain
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.TimeToLive
+import org.springframework.data.redis.core.index.Indexed
 
 /**
  * 재발급 토큰 정보를 저장하는 RedisHash 클래스 입니다.
@@ -10,8 +11,11 @@ import org.springframework.data.redis.core.TimeToLive
 @RedisHash
 data class RefreshToken(
     @Id
-    val email: String,
     val token: String,
+
+    @Indexed
+    val email: String,
+
     @TimeToLive
     val expirationTime: Int
 )
